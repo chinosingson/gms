803c63817c03ee7a7411d6fc7592d6e44dee258a
Richie Lo <funchum12@gmail.com>
2015-06-29 09:21:11 +0800

    simplified

--- sites/all/themes/bootstrap/gms/js/leaflet/app.js
+++ sites/all/themes/bootstrap/gms/js/leaflet/app.js
@@ -3,27 +3,6 @@ jQuery(document).ready(function(){
 		var map;
 		var ctiMapCenter = new google.maps.LatLng(9.622, 111.137);
 
-		function HomeControl(controlDiv, map){
-			/* code for Center custom control */
-		  // Set CSS styles for the DIV containing the control
-		  // Setting padding to 5 px will offset the control
-		  // from the edge of the map
-		  controlDiv.style.padding = '5px';
-
-		  // Set CSS for the control border
-		  var controlUI = document.createElement('div');
-		  controlUI.style.backgroundColor = 'white';
-		  controlUI.style.borderStyle = 'solid';
-		  controlUI.style.borderWidth = '1px';
-		  controlUI.style.borderColor = '#aaa';
-		  controlUI.style.cursor = 'pointer';
-		  controlUI.style.textAlign = 'center';
-		  controlUI.title = 'Click to set the map to Home';
-		  controlDiv.appendChild(controlUI);
-		  });
-			/* code for Center custom control */
-		}
-
 		function initialize() {
 			var mapDiv = document.getElementById('map-canvas');
 			var markers = [];
@@ -64,15 +43,8 @@ jQuery(document).ready(function(){
 				google.maps.event.trigger(map, 'resize');
 				map.setCenter(center); 
 			});
-
-			// Create the DIV to hold the control and
-			// call the homeControl() constructor passing
-			// in this DIV.
-			var homeControlDiv = document.createElement('div');
-			var homeControl = new HomeControl(homeControlDiv, map);
-
-			homeControlDiv.index = 2;
-			map.controls[google.maps.ControlPosition.LEFT_CENTER].push(homeControlDiv);
+					
+			
 										
 		}
 	

36bb8c5ef4d59166766421ac69209ed7de670d10
Richie Lo <funchum12@gmail.com>
2015-07-02 17:18:43 +0800

    Added gms_misc module

---
+++ sites/all/modules/gms_misc/gms_misc.js
@@ -0,0 +1,48 @@
+﻿(function ($) {
+	Drupal.behaviors.map_json = {
+		attach: function(context, settings) {
+/*
+			 $.post("/gms/gms_misc_map",
+				{
+					name: "Donald Duck",
+					city: "Duckburg"
+				},
+				function(data, status){
+					alert("Data: " + data + "\nStatus: " + status);
+				}); 
+*//*
+				$.ajax({ 
+				  type: 'POST', 
+				  url: '/gms/gms_misc_map', 
+				  dataType: 'json', 
+				  data:  {
+					name: "Donald Duck",
+					city: "Duckburg"
+				},
+					success:function(data) { 
+					alert(data.status); 
+					},
+					complete: function(data){
+					console.log(data + status)  
+				}
+				}); 
+				*/
+/*				
+			  $.ajax({
+				type: 'POST',
+				url: '/gms/gms_misc_map',
+				dataType: 'json',
+
+			   data: 'js=1&my_module_post_data=2012'
+			  });
+
+  */ 
+$.post( "/gms/gms_misc_map", { name: "John", time: "2pm" })
+  .done(function( data ) {
+    console.log( "Data Loaded: " + data );
+  });
+	
+				
+		}
+	};
+})(jQuery);
